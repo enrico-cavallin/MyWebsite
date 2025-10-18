b6d63f654732d03f8081b042a7758934681a3761
enrico-cavallin <enrico.cavallin@icloud.com>
2025-10-18 09:53:56 +0200

    Add files via upload
    Signed-off-by: enrico-cavallin <enrico.cavallin@icloud.com>

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ function setLanguage(lang) {
   document.querySelectorAll('[data-en]').forEach(el => {
     el.textContent = el.getAttribute(`data-${lang}`);
   });
+
 }
 
 // Toggle between English and Italian
