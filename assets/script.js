b8597e42b2c7ca673806447cafc2a7312b55b65e
enrico-cavallin <enrico.cavallin@icloud.com>
2025-10-22 20:19:33 +0200

    Add files via upload
    Signed-off-by: enrico-cavallin <enrico.cavallin@icloud.com>

--- assets/script.js
+++ assets/script.js
@@ -84,7 +84,7 @@ if(window.matchMedia("(max-width: 1200px)").matches && window.location.pathname.
 
   document.addEventListener("firstTabSelected", function(){
     const hero = document.querySelector(".hero");
-    hero.style.height="120%"
+    hero.style.height="110%"
   });
 }
 
